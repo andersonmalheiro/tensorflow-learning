6107ccb369e5182c9dee0c199ac93fdabe8438c4
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2019-05-05 09:15:29 -0300

    Basic Classification - Adding training step

--- basic_classification.py
+++ basic_classification.py
@@ -6,19 +6,15 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Carrega a base de dados de imagens
 fashion_mnist = keras.datasets.fashion_mnist
 
+# Divide os dados em base de treinamento e testes
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+# Labels
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
@@ -28,7 +24,21 @@ model = keras.Sequential([
     # Camada para converter as imagens em vetores 1D de 28x28=784 pixels
     keras.layers.Flatten(input_shape=(28, 28)),
 
-    # Camada 
+    # Camada com 128 neurônios
     keras.layers.Dense(128, activation=tf.nn.relu),
+
+    # Camada com 10 nós de saída que calculam a probabilidade
+    # de uma imagem pertencer a uma das categorias
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
+
+# Compila o modelo, passando função de custo, otimizado e métricas
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Passo de treinamento
+model.fit(train_images, train_labels, epochs=5)
+
+# Avaliação do modelo
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy: ', test_acc)
